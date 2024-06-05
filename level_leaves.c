6328b724d0dfcaf98417a600d24837013545011c
Horia <ptphoria@proton.me>
2024-06-05 21:22:04 +0300

    update

--- level_leaves.c
+++ level_leaves.c
@@ -1,122 +1,90 @@
-// Se da un arbore binar oarecare, ın care frunzele se pot afla pe
-// oricare nivel.
-// Scrieti un algoritm care introduce noduri dummy ın arbore astfel
-// ıncat frunzele sa ajunga toate pe acelasi nivel.
 #include <stdio.h>
 #include <stdlib.h>
 
-struct TreeNode
+typedef struct Node
 {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
 
-typedef struct TreeNode TreeNode;
-
-void print_level_for_everyleaf(TreeNode *root, int level, int *maxlevel)
+Node *createNode(int data)
 {
-    if (root->left == NULL)
-    {
-        if (root->right == NULL)
-        {
-            printf("Leaf %d is at level %d\n", root->val, level);
-            if (level > *maxlevel)
-            {
-                *maxlevel = level;
-            }
-        }
-        else
-        {
-            print_level_for_everyleaf(root->right, level + 1, maxlevel);
-        }
-    }
-    else
-    {
-        if (root->right != NULL)
-        {
-            print_level_for_everyleaf(root->right, level + 1, maxlevel);
-        }
-        print_level_for_everyleaf(root->left, level + 1, maxlevel);
-    }
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
 
-void add_levels_to_leaves(TreeNode *root, int level, int maxlevel)
-{
-    if (root->left == NULL)
-    {
-        if (root->right == NULL)
-        {
-            while (level < maxlevel)
-            {
-                TreeNode *dummy = (TreeNode *)malloc(sizeof(TreeNode));
-                dummy->val = -1;
-                dummy->left = NULL;
-                dummy->right = NULL;
-                root->left = dummy;
-                level++;
-                root = root->left;
-            }
-        }
-        else
-        {
-            add_levels_to_leaves(root->right, level + 1, maxlevel);
-        }
-    }
-    else
-    {
-        if (root->right != NULL)
-        {
-            add_levels_to_leaves(root->right, level + 1, maxlevel);
-        }
-        add_levels_to_leaves(root->left, level + 1, maxlevel);
-    }
-}
-
-void print_tree_on_levels(TreeNode *root)
+void dfs(Node *root)
 {
     if (root == NULL)
     {
         return;
     }
-    printf("%d ", root->val);
-    print_tree_on_levels(root->left);
-    print_tree_on_levels(root->right);
+    printf("%d ", root->data);
+    dfs(root->left);
+    dfs(root->right);
 }
 
-void level_allleaves(TreeNode *root)
+int getHeight(Node *root)
 {
-    int maxlevel = 0;
-    print_level_for_everyleaf(root, 0, &maxlevel);
-    printf("Max level is %d\n", maxlevel);
-    add_levels_to_leaves(root, 0, maxlevel);
-    print_tree_on_levels(root);
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int leftHeight = getHeight(root->left);
+    int rightHeight = getHeight(root->right);
+
+    return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+}
+
+void createDummies(Node *root, int height)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    if (height > 1)
+    {
+        if (root->left == NULL)
+        {
+            root->left = createNode(-1);
+        }
+        if (root->right == NULL)
+        {
+            root->right = createNode(-1);
+        }
+    }
+
+    createDummies(root->left, height - 1);
+    createDummies(root->right, height - 1);
 }
 
 int main()
 {
-    int maxlevel = 0;
-    TreeNode *root = (TreeNode *)malloc(sizeof(TreeNode));
-    root->val = 1;
-    root->left = (TreeNode *)malloc(sizeof(TreeNode));
-    root->left->val = 2;
-    root->left->left = NULL;
-    root->left->right = NULL;
-    root->right = (TreeNode *)malloc(sizeof(TreeNode));
-    root->right->val = 3;
-    root->right->left = (TreeNode *)malloc(sizeof(TreeNode));
-    root->right->left->val = 4;
-    root->right->left->left = NULL;
-    root->right->left->right = NULL;
-    root->right->right = (TreeNode *)malloc(sizeof(TreeNode));
-    root->right->right->val = 5;
-    root->right->right->left = NULL;
-    root->right->right->right = NULL;
-    root->right->right->right = (TreeNode *)malloc(sizeof(TreeNode));
-    root->right->right->right->val = 6;
-    root->right->right->right->left = NULL;
-    root->right->right->right->right = NULL;
+    Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    // root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
 
-    level_allleaves(root);
+    root->left->right->left = createNode(8);
+
+    //        1
+    //    2       3
+    //  _   5   6   7
+    // _ _ 8 _ _ _ _ _
+
+    dfs(root);
+    printf("\n");
+    createDummies(root, getHeight(root));
+    dfs(root);
+
     return 0;
 }
